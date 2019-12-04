11d2fb7859881eac6c2a8d0a29d58ac76c3d3fe9
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-05 00:40:29 +0900

    update form.component.spec.ts

--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -37,4 +37,22 @@ describe('FormComponent', () => {
   it('should compile', () => {
     expect(component).toBeTruthy();
   });
+
+  it('First name：必須チェックのテスト', () => {
+    const firstName = component.addressForm.controls['firstName'];
+    firstName.markAllAsTouched();
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('mat-error').textContent).toContain('First name is required');
+  });
+
+  it('Last name：必須チェックのテスト', () => {
+    const lastName = component.addressForm.controls['lastName'];
+    lastName.markAllAsTouched();
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('mat-error').textContent).toContain('Last name is required');
+  });
 });

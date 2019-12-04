f0daa074a14ce7ebc22178a03be615afa4d41753
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-05 01:26:42 +0900

    update

--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -1,11 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
+
+import { DemoMaterialModule } from '../material-module';
 
 import { FormComponent } from './form.component';
 
@@ -19,11 +16,7 @@ describe('FormComponent', () => {
       imports: [
         NoopAnimationsModule,
         ReactiveFormsModule,
-        MatButtonModule,
-        MatCardModule,
-        MatInputModule,
-        MatRadioModule,
-        MatSelectModule,
+        DemoMaterialModule
       ]
     }).compileComponents();
   }));

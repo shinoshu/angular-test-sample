07511e57c8a1e0676c2100f2c0ba514ba310c4b6
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-06 00:13:51 +0900

    update

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
@@ -16,7 +17,7 @@ export class TableComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator, {static: false}) paginator!: MatPaginator;
   @ViewChild(MatSort, {static: false}) sort!: MatSort;
   @ViewChild(MatTable, {static: false}) table!: MatTable<TableItem>;
-  dataSource!: TableDataSource;
+  dataSource!: MatTableDataSource<any>;
 
   users$!: Observable<any>;
 
@@ -28,13 +29,15 @@ export class TableComponent implements AfterViewInit, OnInit {
   ) {}
 
   ngOnInit() {
-    // this.dataSource = new TableDataSource();
-    this.users$ = this.apiClientService.list('users');
+    this.dataSource = new MatTableDataSource<any>();
+    this.apiClientService.list('users').subscribe(users => {
+      this.dataSource.data = users;
+    });
   }
 
   ngAfterViewInit() {
-    // this.dataSource.sort = this.sort;
-    // this.dataSource.paginator = this.paginator;
-    // this.table.dataSource = this.dataSource;
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
   }
 }

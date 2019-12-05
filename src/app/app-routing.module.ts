92fa0c9b2ecf7a2f5967a172294367308ad563b6
Shuma Shinozaki <shu.shinozaki@gmail.com>
2019-12-05 23:06:43 +0900

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TableComponent } from './table/table.component';
 import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
+  { path: 'table', component: TableComponent },
   { path: 'form', component: FormComponent }
 ];
 
